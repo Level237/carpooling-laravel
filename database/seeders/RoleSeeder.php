48bfba1a049afb905c7ac8186eb2131e6083b873
level237 <Bramslevel129@gmail.com>
2024-06-16 13:58:19 +0100

    update seeder

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Role;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,22 @@ class RoleSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        Role::create([
+            'id'=>1,
+            'title'=>'admin'
+        ],
+    );
+
+    Role::create([
+        'id'=>2,
+        'title'=>'driver'
+    ],
+    );
+
+    Role::create([
+        'id'=>3,
+        'title'=>'client'
+    ],
+    );
     }
 }
--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,31 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        User::create([
+            'id'=>1,
+            'name'=>"Admin",
+            'email'=>"admin@driver.com",
+            'role_id'=>1,
+            'password'=>bcrypt('password'),
+        ],
+        );
+
+        User::create([
+            'id'=>2,
+            'name'=>"alain",
+            'email'=>"Alain@gmail.com",
+            'role_id'=>2,
+            'password'=>bcrypt('password'),
+        ],
+        );
+
+        User::create([
+            'id'=>3,
+            'name'=>"user",
+            'email'=>"user@gmail.com",
+            'role_id'=>3,
+            'password'=>bcrypt('password'),
+        ],
+        );
     }
 }
