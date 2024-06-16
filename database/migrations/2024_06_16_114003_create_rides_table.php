22088610d9d2f2c061b850b76a09dc9e361ef952
level237 <Bramslevel129@gmail.com>
2024-06-16 13:08:23 +0100

    breeze in action

--- database/migrations/2024_06_16_114003_create_rides_table.php
+++ database/migrations/2024_06_16_114003_create_rides_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -13,6 +14,14 @@ return new class extends Migration
     {
         Schema::create('rides', function (Blueprint $table) {
             $table->id();
+            $table->foreignIdFor(User::class)
+            ->constrained()
+            ->cascadeOnUpdate()
+            ->cascadeOnDelete();
+            $table->string('departure_location');
+            $table->string('arrival_departure');
+            $table->string('departure_time');
+            $table->string('available_seats');
             $table->timestamps();
         });
     }
